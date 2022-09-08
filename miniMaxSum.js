f3b1b60a9f3b0626726b729603189bd0365c2b2d
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-09-08 15:53:30 -0500

    update

--- miniMaxSum.js
+++ miniMaxSum.js
@@ -23,6 +23,7 @@
 
 // Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
 
+// Solution 1
 let arr = [1, 2, 3, 4, 5];
 let min = arr[0];
 let max = arr[0];
@@ -42,5 +43,22 @@ function miniMaxSum(arr) {
   maxSum = sum - min;
   minSum = sum - max;
   console.log(minSum, maxSum);
+}
+miniMaxSum(arr);
+
+// Solution 2
+let arr = [1, 2, 3, 4, 5];
+
+function miniMaxSum(arr) {
+  // let newArr = [...arr].sort();
+  let newArr = arr.sort();
+  let sum = 0;
+
+  for (let i = 0; i < newArr.length; i++) {
+    sum += newArr[i];
+  }
+  let max = sum - newArr[0];
+  let min = sum - newArr[newArr.length - 1];
+  console.log(min, max);
 }
 miniMaxSum(arr);
