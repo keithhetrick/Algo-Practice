67ef1e6ed0f708c7a114ba328df39732ee312702
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-01 19:18:47 -0500

    new folder structure

---
+++ CodeSignal/addTwoDigits.js
@@ -0,0 +1,37 @@
+// Add Two Digits
+
+// Codewriting  300
+
+// You are given a two-digit integer n. Return the sum of its digits.
+
+// Example
+// For n = 29, the output should be
+// solution(n) = 11.
+
+// Input/Output
+// [execution time limit] 4 seconds (js)
+
+// [input] integer n
+
+// A positive two-digit integer.
+
+// Guaranteed constraints:
+// 10 ≤ n ≤ 99.
+
+// [output] integer
+
+// The sum of the first and second digits of the input number.
+
+function solution(n) {
+  let sum = 0;
+
+  let newArr = String(n)
+    .split("")
+    .map((n) => Number(n));
+  for (i = 0; i < newArr.length; i++) {
+    sum += newArr[i];
+  }
+  console.log(sum);
+  return sum;
+}
+solution(29);
---
+++ CodeSignal/largestNumber.js
@@ -0,0 +1,26 @@
+// Largest Number
+
+// Given an integer n, return the largest number that contains exactly n digits.
+
+// Example
+// For n = 2, the output should be
+// solution(n) = 99.
+
+// Input/Output
+// [execution time limit] 4 seconds (js)
+
+// [input] integer n
+
+// Guaranteed constraints:
+// 1 ≤ n ≤ 9.
+
+// [output] integer
+
+// The largest integer of length n.
+
+// str.split('').join(' ')
+
+function solution(n) {
+  return (maxNum = Math.pow(10, n) - 1);
+}
+solution(3);
