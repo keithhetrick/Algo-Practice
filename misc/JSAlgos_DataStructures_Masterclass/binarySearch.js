43c3aebb3ba025fc185d628d3776fe30b3c7bbee
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-05 16:49:31 -0500

    binarySearch

--- misc/JSAlgos_DataStructures_Masterclass/binarySearch.js
+++ misc/JSAlgos_DataStructures_Masterclass/binarySearch.js
@@ -6,9 +6,9 @@ function search(array, val) {
     let middle = Math.floor((min + max) / 2);
     let currentElement = array[middle];
 
-    if (array[middle] < val) {
+    if (currentElement < val) {
       min = middle + 1;
-    } else if (array[middle] > val) {
+    } else if (currentElement > val) {
       max = middle - 1;
     } else {
       return middle;
---
+++ misc/JSAlgos_DataStructures_Masterclass/search-binarySearch.js
@@ -0,0 +1,60 @@
+// Binary Search Exercise
+
+// Function accepts a sorted array and a value
+// Create a left pointer at the start of the array, and a right pointer at the end of the array
+// While the left pointer comes before the right pointer:
+// Create a pointer in the middle
+// If you find the value you want, return the index
+// If the value is too small, move the left pointer up
+// If the value is too large, move the right pointer down
+// If you never find the value, return -1
+
+function binarySearch(arr, val) {
+  let min = 0;
+  let max = arr.length - 1;
+
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2);
+    let currentElement = arr[middle];
+
+    if (currentElement < val) {
+      min = middle + 1;
+    } else if (currentElement > val) {
+      max = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+  return -1;
+}
+console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
+console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    10
+  )
+); // 2
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    95
+  )
+); // 16
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    100
+  )
+); // -1
