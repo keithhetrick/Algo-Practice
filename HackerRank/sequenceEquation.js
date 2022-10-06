a439a572226e2fea2ce7405561e2454a65166a37
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-05 23:44:27 -0500

    binarySearch exercises & sequenceEquation start

---
+++ HackerRank/sequenceEquation.js
@@ -0,0 +1,13 @@
+function permutationEquation(p) {
+  // let x = [] //
+  // let newArr = [];
+  let y = []; // final values
+  for (let i = 0; i < p.length; i++) {
+    if (p[i] === i) y.push(p[i]);
+    console.log(`current index: ${i}`);
+
+    // y.push(p[i]);
+    console.log(`EQUAL to i: ${i} + y: ${y}`);
+  }
+}
+permutationEquation([4, 3, 5, 1, 2]);
