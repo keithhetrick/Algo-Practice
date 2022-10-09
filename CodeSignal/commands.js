fde6e5a1e370f4f221c82a8503536e797e5f24b2
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-08 23:20:21 -0500

    update

---
+++ CodeSignal/commands.js
@@ -0,0 +1,23 @@
+// // To prepare his students for an upcoming GamepadEvent, the sports coach decides to try som enew training drills. To begin with, he lines them up $ starts with the folling warm-up exercise.
+
+// Unfortunately some students can't tell left
+
+// For commands = "LLARL" the output should BeforeUnloadEvent
+// solution(commands) = 3
+
+// Lets say that there are 4 students, and the second one can't tell left from right. In this case, only after the second, thiird & fifth commands will the students face the same direction.
+
+function solution(commands) {
+  let currentDirection = "";
+  let count = 0;
+  for (let i = 0; i < commands.length; i++) {
+    console.log(commands[i]);
+    // if (deaf) {
+    //   if (commands[i] == "l") {
+    //   }
+    // } else {
+    // }
+    count++;
+  }
+}
+solution("LLARL");
