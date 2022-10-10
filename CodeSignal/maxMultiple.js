aa33c957893f1b50300a27489182c2556d63b9eb
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-10 12:49:03 -0500

    maxMultiple

--- CodeSignal/maxMultiple.js
+++ CodeSignal/maxMultiple.js
@@ -1,4 +1,4 @@
-// Max Multiple
+// 5. Max Multiple
 
 // Given a divisor and a bound, find the largest integer N such that:
 
@@ -30,13 +30,16 @@ function solution(divisor, bound) {
   let sum = remainder * divisor;
   return sum;
 }
-solution(3, 10); // expect 9
-solution(2, 7); // expect 6
-solution(10, 50); // expect 50
-solution(7, 100); // expect 98
-solution(7, 20); // expect 14
-solution(5, 13); // expect 10
-solution(8, 88); // expect 88
-solution(10, 100); // expect 100
-solution(2, 5); // expect 4
-solution(3, 5); // expect 3
+solution(3, 10); // expects 9
+solution(2, 7); // expects 6
+solution(10, 50); // expects 50
+solution(7, 100); // expects 98
+solution(7, 20); // expects 14
+solution(5, 13); // expects 10
+solution(8, 88); // expects 88
+solution(10, 100); // expects 100
+solution(2, 5); // expects 4
+solution(3, 5); // expects 3
+solution(9, 11); // expects 9
+solution(8, 15); // expects 8
+solution(9, 60); // expects 54
