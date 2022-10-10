866e2d5ca853f8d516e7edc1fa3ac8e332b50438
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-10 12:06:28 -0500

    maxMultiple

---
+++ CodeSignal/maxMultiple.js
@@ -0,0 +1,42 @@
+// Max Multiple
+
+// Given a divisor and a bound, find the largest integer N such that:
+
+// N is divisible by divisor.
+// N is less than or equal to bound.
+// N is greater than 0.
+// It is guaranteed that such a number exists.
+
+// Example
+// For divisor = 3 and bound = 10, the output should be
+// solution(divisor, bound) = 9.
+// The largest integer divisible by 3 and not larger than 10 is 9.
+
+// Input/Output
+// [execution time limit] 4 seconds (js)
+// [input] integer divisor
+
+// Guaranteed constraints:
+// 2 ≤ divisor ≤ 10.
+// [input] integer bound
+
+// Guaranteed constraints:
+// 5 ≤ bound ≤ 100.
+// [output] integer
+// The largest integer not greater than bound that is divisible by divisor.
+
+function solution(divisor, bound) {
+  let remainder = Math.floor(bound / divisor);
+  let sum = remainder * divisor;
+  return sum;
+}
+solution(3, 10); // expect 9
+solution(2, 7); // expect 6
+solution(10, 50); // expect 50
+solution(7, 100); // expect 98
+solution(7, 20); // expect 14
+solution(5, 13); // expect 10
+solution(8, 88); // expect 88
+solution(10, 100); // expect 100
+solution(2, 5); // expect 4
+solution(3, 5); // expect 3
