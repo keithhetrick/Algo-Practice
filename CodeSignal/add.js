3f90bd468e7d489f3a15b31723115d8f19b0cb1f
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-12 22:26:47 -0500

    add

---
+++ CodeSignal/add.js
@@ -0,0 +1,9 @@
+function solution(param1, param2) {
+  return param1 + param2;
+}
+solution(1, 2); // expects 3
+solution(0, 1000); // expects 1000
+solution(2, -39); // expects -37
+solution(99, 100); // expects 199
+solution(-100, 100); // expects 0
+solution(-1000, -1000); // expects -2000
