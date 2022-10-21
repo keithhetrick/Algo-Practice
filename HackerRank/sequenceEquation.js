0863002baed6ffba32a43aa27c6647fae3afa94c
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-21 10:16:52 -0500

    sequenceEquation

--- HackerRank/sequenceEquation.js
+++ HackerRank/sequenceEquation.js
@@ -1,22 +1,12 @@
 // Sequence Equation
+// https://www.hackerrank.com/challenges/permutation-equation/problem
 
 function permutationEquation(p) {
-  let j = [];
-  let y = [];
-  for (let x = 1; x < 6; x++) {
-    for (let i = 0; i < p.length; i++) {
-      if (p[i] === x) j.push(i + 1);
-    }
+  let result = [];
+  for (let i = 1; i <= p.length; i++) {
+    result.push(p.indexOf(p.indexOf(i) + 1) + 1);
   }
-
-  for (let x = 1; x < 6; x++) {
-    for (let i = 0; i < j.length; i++) {
-      if (p[i] === j[x - 1]) y.push(p[i]);
-    }
-  }
-
-  // y.sort((a,b) => a-b)
-  return y;
+  return result;
 }
 permutationEquation([2, 3, 1]); // expects 2,3,1
 permutationEquation([4, 3, 5, 1, 2]); // expects 1,3,5,4,2
