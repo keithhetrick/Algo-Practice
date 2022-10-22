654d6766feb57e127cebee34d7394c744e600226
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-22 00:12:29 -0500

    threeNumberSum

---
+++ misc/threeNumberSum.js
@@ -0,0 +1,21 @@
+// Three Number Sum
+
+function threeNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  const triplets = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    let left = i + 1;
+    let right = array.length - 1;
+    while (left < right) {
+      const currentSum = array[i] + array[left] + array[right];
+      if (currentSum === targetSum) {
+        triplets.push([array[i], array[left], array[right]]);
+        left++;
+        right--;
+      } else if (currentSum < targetSum) left++;
+      else if (currentSum > targetSum) right--;
+    }
+  }
+  return triplets;
+}
+threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0); // expects [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
