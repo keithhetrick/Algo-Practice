b785c9699f02ec913c71791c7b3b5d5c17a09062
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-22 10:38:56 -0500

    reversedPolishNotation

---
+++ LeetCode/reversedPolishNotation.js
@@ -0,0 +1,25 @@
+// ReveresedPolishNotation
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+function rpn(tokens) {
+  let stack = [];
+  let operators = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => parseInt(a / b),
+  };
+  for (let token of tokens) {
+    if (token in operators) {
+      let b = stack.pop();
+      let a = stack.pop();
+      stack.push(operators[token](a, b));
+    } else {
+      stack.push(parseInt(token));
+    }
+  }
+  return stack.pop();
+}
+rpn(["2", "1", "+", "3", "*"]); // expects 9
+rpn(["4", "13", "5", "/", "+"]); // expects 6
+rpn(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]); // expects 22
