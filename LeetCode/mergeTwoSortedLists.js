2ebc2b1bd0ff0851f47b0ea704f5a99ee024e1ec
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-24 08:39:47 -0500

    mergeTwoSortedLists

---
+++ LeetCode/mergeTwoSortedLists.js
@@ -0,0 +1,36 @@
+// 21. Merge Two Sorted Lists
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+const mergeTwoLists = (list1, list2) => {
+  if (!list1) return list2;
+  if (!list2) return list1;
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
+  }
+};
+mergeTwoLists([1, 2, 4], [1, 3, 4]); // expects [1,1,2,3,4,4]
+mergeTwoLists([], []); // expects []
+mergeTwoLists([], [0]); // expects [0]
