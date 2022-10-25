0897682e49e450f84257eef953d55e7e3f8e3408
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-25 14:42:56 -0500

    arrayOdd

---
+++ CodingDojo/arrayOdd.js
@@ -0,0 +1,13 @@
+// Challenge 7: Array Odd
+
+// Write a function that would return an array of all the odd numbers between 1 to 50. (ex. [1,3,5, .... , 47,49]). Hint: Use 'push' method.
+
+function oddNumbers() {
+  let arr = [];
+
+  for (let i = 1; i < 50; i++) {
+    if (i % 2 === 1) arr.push(i);
+  }
+  return arr;
+}
+oddNumbers(); // expects [1, 3, 5, ..., 47, 49]
