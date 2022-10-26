7d304a1adb377fa9e96d18887ba9b220939c6d4e
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-26 12:45:25 -0500

    dojoPizzeria

---
+++ CodingDojo/dojoPizzeria.js
@@ -0,0 +1,43 @@
+// Dojo Pizzeria
+
+function pizzaOven(crustType, sauceType, cheeses, toppings) {
+  const pizza = {};
+
+  pizza.crustType = crustType;
+  pizza.sauceType = sauceType;
+  pizza.cheeses = cheeses;
+  pizza.toppings = toppings;
+  return pizza;
+}
+
+let pizza1 = pizzaOven(
+  "deep dish",
+  "traditional",
+  ["mozzarella"],
+  ["pepperoni", "sausage"]
+);
+console.log(pizza1);
+
+let pizza2 = pizzaOven(
+  "hand tossed",
+  "marinara",
+  ["mozzarella", "feta"],
+  ["mushrooms", "olives", "onions"]
+);
+console.log(pizza2);
+
+let pizza3 = pizzaOven(
+  "thin crust",
+  "pesto",
+  ["parmesan"],
+  ["ham", "pineapple"]
+);
+console.log(pizza3);
+
+let pizza4 = pizzaOven(
+  "deep dish",
+  "traditional",
+  ["mozzarella"],
+  ["pepperoni", "sausage"]
+);
+console.log(pizza4);
