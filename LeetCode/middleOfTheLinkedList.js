1f521ff7ac5ff4d73fb06f917b7d3b680784a368
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-26 22:04:03 -0500

    middleOfTheLinkedList

---
+++ LeetCode/middleOfTheLinkedList.js
@@ -0,0 +1,19 @@
+// 876. Middle of the Linked List
+// https://leetcode.com/problems/middle-of-the-linked-list/
+
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+function middleNode(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+}
+middleNode([1, 2, 3, 4, 5]); // expects [3,4,5]
+middleNode([1, 2, 3, 4, 5, 6]); // expects [4,5,6]
