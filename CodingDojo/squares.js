16c56ef6e19f941b590c7ca284eb006ff3184230
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-29 22:45:21 -0500

    squares

--- CodingDojo/squares.js
+++ CodingDojo/squares.js
@@ -3,9 +3,8 @@
 // Given an array with multiple values, write a function that replaces each value in the array with the product of the original value multiplied by itself. (e.g. [1,5,10,-2] will become [1,25,100,4])
 
 function squareVal(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = Math.pow(arr[i], 2);
-  }
+  for (let i = 0; i < arr.length; i++) arr[i] = Math.pow(arr[i], 2);
+
   return arr;
 }
 squareVal([1, 2, 3]); // expects [1,4,9]
