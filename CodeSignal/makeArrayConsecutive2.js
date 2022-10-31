7775c2eca639fbaba16563d28e8a1abee4babbcf
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-10-30 21:54:09 -0500

    makeArrayConsecutive2

--- CodeSignal/makeArrayConsecutive2.js
+++ CodeSignal/makeArrayConsecutive2.js
@@ -8,6 +8,7 @@
 function solution(statues) {
   let min = Math.min(...statues);
   let max = Math.max(...statues);
+
   let count = 0;
   for (let i = min; i < max; i++) {
     if (!statues.includes(i)) {
