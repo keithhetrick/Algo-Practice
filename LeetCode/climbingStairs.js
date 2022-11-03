f7c0df4f6ded9e146536c6685a9ef66ae87b5e42
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-03 15:54:36 -0500

    climbingStairs

---
+++ LeetCode/climbingStairs.js
@@ -0,0 +1,13 @@
+// 70. Climbing Stairs
+// https://leetcode.com/problems/climbing-stairs/
+
+function climbStairs(n) {
+  let a = 1;
+  let b = 1;
+  for (let i = 2; i <= n; i++) {
+    [a, b] = [b, a + b];
+  }
+  return b;
+}
+climbStairs(2); // expects 2
+climbStairs(3); // expects 3
