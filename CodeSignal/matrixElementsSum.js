761643b493eb122527cc2b837e84a318f6f40cba
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-04 16:28:10 -0500

    matrixElementsSum

--- CodeSignal/matrixElementsSum.js
+++ CodeSignal/matrixElementsSum.js
@@ -4,6 +4,7 @@
 function solution(matrix) {
   let sum = 0;
   let haunted = [];
+
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j] === 0) haunted.push(j);
