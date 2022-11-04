6bc635a79ba8ac40925b7d6fdd1faca1cf02116c
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-04 16:27:03 -0500

    centuryFromYear

--- CodeSignal/centuryFromYear.js
+++ CodeSignal/centuryFromYear.js
@@ -1,4 +1,4 @@
-//  Century From Year
+// 2. Century From Year
 
 // Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
 
