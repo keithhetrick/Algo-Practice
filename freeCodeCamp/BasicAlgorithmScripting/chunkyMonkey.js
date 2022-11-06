cec7bd03df1eaae372396d55edd7608fb0b63dc6
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-06 16:06:06 -0600

    chunkyMonkey

---
+++ freeCodeCamp/BasicAlgorithmScripting/chunkyMonkey.js
@@ -0,0 +1,17 @@
+// Chunky Monkey
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/chunky-monkey
+
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
+chunkArrayInGroups(["a", "b", "c", "d"], 2); // expects [["a", "b"], ["c", "d"]]
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3); // expects [[0, 1, 2], [3, 4, 5]]
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2); // expects [[0, 1], [2, 3], [4, 5]]
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4); // expects [[0, 1, 2, 3], [4, 5]]
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3); // expects [[0, 1, 2], [3, 4, 5], [6]]
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4); // expects [[0, 1, 2, 3], [4, 5, 6, 7], [8]]
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2); // expects [[0, 1], [2, 3], [4, 5], [6, 7], [8]]
