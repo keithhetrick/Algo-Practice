9eb262ca724e4a9745883feb3cc1c302e9d48866
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-06 16:14:15 -0600

    BasicDataStructures

---
+++ freeCodeCamp/BasicDataStructures/createBasicJSObject.js
@@ -0,0 +1,7 @@
+// Create a Basic JavaScript Object
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/object-oriented-programming/create-a-basic-javascript-object
+
+let dog = {
+  name: "Ollie",
+  numLegs: 4,
+};
