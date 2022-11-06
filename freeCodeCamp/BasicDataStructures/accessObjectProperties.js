cd9eafa2b3cdeaf099a8a90a537cc19735cd8bd4
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-06 17:25:51 -0600

    accessObjectProperties

---
+++ freeCodeCamp/BasicDataStructures/accessObjectProperties.js
@@ -0,0 +1,9 @@
+// Use Dot Notation to Access the Properties of an Object
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/object-oriented-programming/use-dot-notation-to-access-the-properties-of-an-object
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+};
+console.log(dog.name);
+console.log(dog.numLegs);
