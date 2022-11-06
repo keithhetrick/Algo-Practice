0a5b77896f3e1cd7c3d004d50427ddbacb0f69c9
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-06 16:12:42 -0600

    chunkyMonkey

--- freeCodeCamp/BasicAlgorithmScripting/chunkyMonkey.js
+++ freeCodeCamp/BasicAlgorithmScripting/chunkyMonkey.js
@@ -2,6 +2,14 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/chunky-monkey
 
 function chunkArrayInGroups(arr, size) {
+  // While loop
+  let groups = [];
+  while (arr.length > 0) {
+    groups.push(arr.splice(0, size));
+  }
+  return groups;
+
+  // For loop
   let newArr = [];
   for (let i = 0; i < arr.length; i += size) {
     newArr.push(arr.slice(i, i + size));
