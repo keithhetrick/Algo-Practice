f0981a0b786cae20250f9cf9b0223c8870cb5ca3
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-06 13:30:51 -0600

    conversionPractice

--- misc/misc-Practice/conversionPractice.js
+++ misc/misc-Practice/conversionPractice.js
@@ -172,3 +172,27 @@ function reverseString(str) {
   return str.split("").reverse().join("");
 }
 reverseString("aeiou");
+
+function confirmEnding(str, target) {
+  // One Liner Solution 1
+  return str.includes(target);
+
+  // One Liner Solution 2
+  return str.slice(str.length - target.length) === target;
+}
+confirmEnding("Bastian", "n"); // expects true
+confirmEnding("Congratulation", "on"); // expects true
+confirmEnding("Connor", "n"); // expects false
+confirmEnding(
+  "Walking on water and developing software from a specification are easy if both are frozen",
+  "specification"
+); // expects false
+confirmEnding("He has to give me a new name", "name"); // expects true
+confirmEnding("Open sesame", "same"); // expects true
+confirmEnding("Open sesame", "sage"); // expects false
+confirmEnding("Open sesame", "game"); // expects false
+confirmEnding(
+  "If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing",
+  "mountain"
+); // expects false
+confirmEnding("Abstraction", "action"); // expects
