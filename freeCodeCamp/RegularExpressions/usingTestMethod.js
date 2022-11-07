5bf67ecb7786e8bc5ee2859a9d61eb751d059e08
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-06 22:32:51 -0600

    Regex

---
+++ freeCodeCamp/RegularExpressions/usingTestMethod.js
@@ -0,0 +1,6 @@
+// Using the Test Method
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/using-the-test-method
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString);
