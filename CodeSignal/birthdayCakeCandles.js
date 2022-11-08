90efdd0f5e7fbe4a592e368f01f180256ed104c6
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-08 09:41:04 -0600

    birthdayCakeCandles

--- CodeSignal/birthdayCakeCandles.js
+++ CodeSignal/birthdayCakeCandles.js
@@ -28,9 +28,7 @@ function birthdayCakeCandles(candles) {
   let max = Math.max(...candles);
   let count = 0;
   for (let i = 0; i < candles.length; i++) {
-    if (candles[i] === max) {
-      count++;
-    }
+    if (candles[i] === max) count++;
   }
   return count;
 }
