51dbf62cc0adb7ab6e09bfbefc79e10f263869d0
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-16 20:33:14 -0700

    hash map practice

--- misc/misc-Practice/hashMapPractice.js
+++ misc/misc-Practice/hashMapPractice.js
@@ -2,23 +2,46 @@
 
 let start = Date.now();
 
-var smallerNumbersThanCurrent = function (nums) {
+function makeHashMap(nums) {
   let hash = {};
 
   for (let i = 0; i < nums.length; i++) {
     let key = nums[i];
     if (!hash[key]) hash[i] = nums[i];
   }
-  console.log(hash);
+  console.log("Hash: ", hash);
   console.log("Perfomance: ", performance.now(), "ms");
   console.log("Time elapsed: ", Date.now() - start, "ms");
   return hash;
-};
-smallerNumbersThanCurrent([8, 1, 2, 2, 3]); // expects [4,0,1,1,3]
-smallerNumbersThanCurrent([6, 5, 4, 8]); // expects [2,1,0,3]
-smallerNumbersThanCurrent([7, 7, 7, 7]); // expects [0,0,0,0]
+}
+makeHashMap([8, 1, 2, 2, 3]); // expects [4,0,1,1,3]
+makeHashMap([6, 5, 4, 8]); // expects [2,1,0,3]
+makeHashMap([7, 7, 7, 7]); // expects [0,0,0,0]
 
-var smallerNumbersThanCurrent = function (nums) {
+// function smallerNumbersThanCurrent(nums) {
+//   let hash = {};
+//   let result = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     let key = nums[i];
+//     if (hash[key]) hash[key]++;
+//     else hash[key] = 1;
+//   }
+//   for (let i = 0; i < nums.length; i++) {
+//     let key = nums[i];
+//     let count = 0;
+//     for (let j = 0; j < key; j++) {
+//       if (hash[j]) count += hash[j];
+//     }
+//     result.push(count);
+//   }
+//   console.log("Smaller: ", result);
+//   return console.log("Time elapsed: ", Date.now() - start, "ms");
+// }
+// smallerNumbersThanCurrent([8, 1, 2, 2, 3]); // expects [4,0,1,1,3]
+// smallerNumbersThanCurrent([6, 5, 4, 8]); // expects [2,1,0,3]
+// smallerNumbersThanCurrent([7, 7, 7, 7]); // expects [0,0,0,0]
+
+function largerNumbersThanCurrent(nums) {
   let hash = {};
   let result = [];
   for (let i = 0; i < nums.length; i++) {
@@ -29,14 +52,23 @@ var smallerNumbersThanCurrent = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     let key = nums[i];
     let count = 0;
-    for (let j = 0; j < key; j++) {
+    for (let j = key + 1; j < nums.length; j++) {
       if (hash[j]) count += hash[j];
     }
     result.push(count);
   }
-  console.log(result);
+  console.log("Larger: ", result);
   return console.log("Time elapsed: ", Date.now() - start, "ms");
-};
-smallerNumbersThanCurrent([8, 1, 2, 2, 3]); // expects [4,0,1,1,3]
-smallerNumbersThanCurrent([6, 5, 4, 8]); // expects [2,1,0,3]
-smallerNumbersThanCurrent([7, 7, 7, 7]); // expects [0,0,0,0]
+}
+largerNumbersThanCurrent([8, 1, 2, 2, 3]); // expects [1,3,2,2,0]
+largerNumbersThanCurrent([6, 5, 4, 8]); // expects [1,1,2,0]
+largerNumbersThanCurrent([7, 7, 7, 7]); // expects [0,0,0,0]
+
+function compareHash(hash1, hash2) {
+  for (let key in hash1) {
+    if (hash1[key] !== hash2[key]) return false;
+  }
+  return true;
+}
+console.log(compareHash({ a: 1, b: 2 }, { a: 1, b: 2 })); // true
+console.log(compareHash({ a: 1, b: 2 }, { a: 1, b: 3 })); // false
