8449b489c96755585f29722df5b76897d82e7302
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-16 20:46:23 -0700

    hash map practice

--- misc/misc-Practice/hashMapPractice.js
+++ misc/misc-Practice/hashMapPractice.js
@@ -70,5 +70,49 @@ function compareHash(hash1, hash2) {
   }
   return true;
 }
-console.log(compareHash({ a: 1, b: 2 }, { a: 1, b: 2 })); // true
-console.log(compareHash({ a: 1, b: 2 }, { a: 1, b: 3 })); // false
+compareHash({ a: 1, b: 2 }, { a: 1, b: 2 }); // true
+compareHash({ a: 1, b: 2 }, { a: 1, b: 3 }); // false
+
+function makeAndCompareHash(nums1, nums2) {
+  let hash1 = {};
+  let hash2 = {};
+  for (let i = 0; i < nums1.length; i++) {
+    let key = nums1[i];
+    if (hash1[key]) hash1[key]++;
+    else hash1[key] = 1;
+  }
+  for (let i = 0; i < nums2.length; i++) {
+    let key = nums2[i];
+    if (hash2[key]) hash2[key]++;
+    else hash2[key] = 1;
+  }
+  return compareHash(hash1, hash2);
+}
+makeAndCompareHash([1, 2, 3], [3, 2, 1]); // true
+makeAndCompareHash([1, 2, 3], [3, 2, 2]); // false
+
+function reassignKeys(hash) {
+  let newHash = {};
+  let keys = Object.keys(hash);
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    newHash[i] = hash[key];
+  }
+  console.log("Reassigned: ", newHash);
+  return newHash;
+}
+reassignKeys({ a: 1, b: 2 }); // {0: 1, 1: 2} // {a: 1, b: 2} => {0: 1, 1: 2}
+reassignKeys({ a: 1, b: 2, c: 3 }); // {0: 1, 1: 2, 2: 3} // {a: 1, b: 2, c: 3} => {0: 1, 1: 2, 2: 3}
+
+function reassignKeysFromDifferentHash(hash1, hash2) {
+  let newHash = {};
+  let keys = Object.keys(hash1);
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    newHash[i] = hash2[key];
+  }
+  console.log("Reassigned: ", newHash);
+  return newHash;
+}
+reassignKeysFromDifferentHash({ a: 1, b: 2 }, { 1: "a", 2: "b" }); // {0: "a", 1: "b"} // {a: 1, b: 2} => {0: "a", 1: "b"}
+reassignKeysFromDifferentHash({ a: 1, b: 2, c: 3 }, { 1: "a", 2: "b", 3: "c" }); // {0: "a", 1: "b", 2: "c"} // {a: 1, b: 2, c: 3} => {0: "a", 1: "b", 2: "c"}
