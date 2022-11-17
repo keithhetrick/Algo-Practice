6011dc094257823130ed3bea76e40ec043e7a565
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-11-17 08:05:48 -0700

    hash map practice

--- misc/misc-Practice/hashMapPractice.js
+++ misc/misc-Practice/hashMapPractice.js
@@ -18,28 +18,28 @@ makeHashMap([8, 1, 2, 2, 3]); // expects [4,0,1,1,3]
 makeHashMap([6, 5, 4, 8]); // expects [2,1,0,3]
 makeHashMap([7, 7, 7, 7]); // expects [0,0,0,0]
 
-// function smallerNumbersThanCurrent(nums) {
-//   let hash = {};
-//   let result = [];
-//   for (let i = 0; i < nums.length; i++) {
-//     let key = nums[i];
-//     if (hash[key]) hash[key]++;
-//     else hash[key] = 1;
-//   }
-//   for (let i = 0; i < nums.length; i++) {
-//     let key = nums[i];
-//     let count = 0;
-//     for (let j = 0; j < key; j++) {
-//       if (hash[j]) count += hash[j];
-//     }
-//     result.push(count);
-//   }
-//   console.log("Smaller: ", result);
-//   return console.log("Time elapsed: ", Date.now() - start, "ms");
-// }
-// smallerNumbersThanCurrent([8, 1, 2, 2, 3]); // expects [4,0,1,1,3]
-// smallerNumbersThanCurrent([6, 5, 4, 8]); // expects [2,1,0,3]
-// smallerNumbersThanCurrent([7, 7, 7, 7]); // expects [0,0,0,0]
+function smallerNumbersThanCurrent(nums) {
+  let hash = {};
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    let key = nums[i];
+    if (hash[key]) hash[key]++;
+    else hash[key] = 1;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    let key = nums[i];
+    let count = 0;
+    for (let j = 0; j < key; j++) {
+      if (hash[j]) count += hash[j];
+    }
+    result.push(count);
+  }
+  console.log("Smaller: ", result);
+  return console.log("Time elapsed: ", Date.now() - start, "ms");
+}
+smallerNumbersThanCurrent([8, 1, 2, 2, 3]); // expects [4,0,1,1,3]
+smallerNumbersThanCurrent([6, 5, 4, 8]); // expects [2,1,0,3]
+smallerNumbersThanCurrent([7, 7, 7, 7]); // expects [0,0,0,0]
 
 function largerNumbersThanCurrent(nums) {
   let hash = {};
@@ -70,8 +70,8 @@ function compareHash(hash1, hash2) {
   }
   return true;
 }
-compareHash({ a: 1, b: 2 }, { a: 1, b: 2 }); // true
-compareHash({ a: 1, b: 2 }, { a: 1, b: 3 }); // false
+compareHash({ a: 1, b: 2 }, { a: 1, b: 2 }); // expects true
+compareHash({ a: 1, b: 2 }, { a: 1, b: 3 }); // expects false
 
 function makeAndCompareHash(nums1, nums2) {
   let hash1 = {};
@@ -86,10 +86,12 @@ function makeAndCompareHash(nums1, nums2) {
     if (hash2[key]) hash2[key]++;
     else hash2[key] = 1;
   }
+  console.log("Hash 1: ", hash1);
+  console.log("Hash 2: ", hash2);
   return compareHash(hash1, hash2);
 }
-makeAndCompareHash([1, 2, 3], [3, 2, 1]); // true
-makeAndCompareHash([1, 2, 3], [3, 2, 2]); // false
+makeAndCompareHash([1, 2, 3], [3, 2, 1]); // expects true
+makeAndCompareHash([1, 2, 3], [3, 2, 2]); // expects false
 
 function reassignKeys(hash) {
   let newHash = {};
@@ -101,8 +103,8 @@ function reassignKeys(hash) {
   console.log("Reassigned: ", newHash);
   return newHash;
 }
-reassignKeys({ a: 1, b: 2 }); // {0: 1, 1: 2} // {a: 1, b: 2} => {0: 1, 1: 2}
-reassignKeys({ a: 1, b: 2, c: 3 }); // {0: 1, 1: 2, 2: 3} // {a: 1, b: 2, c: 3} => {0: 1, 1: 2, 2: 3}
+reassignKeys({ a: 1, b: 2 }); // {0: 1, 1: 2} // expects {a: 1, b: 2} => {0: 1, 1: 2}
+reassignKeys({ a: 1, b: 2, c: 3 }); // expects {0: 1, 1: 2, 2: 3} // {a: 1, b: 2, c: 3} => {0: 1, 1: 2, 2: 3}
 
 function createHashAndReassignKeysToDifferentHash(hash1, hash2) {
   let newHash = {};
@@ -115,4 +117,23 @@ function createHashAndReassignKeysToDifferentHash(hash1, hash2) {
   console.log("Reassigned: ", newHash);
   return newHash;
 }
-createHashAndReassignKeysToDifferentHash({ a: 1, b: 2 }, { a: 1, b: 2 }); // {1: 1, 2: 2} // {a: 1, b: 2} => {1: 1, 2: 2}
+createHashAndReassignKeysToDifferentHash({ a: 1, b: 2 }, { a: 1, b: 2 }); // expects {a: 1, b: 2} => {1: 1, 2: 2}
+
+function createHashAndUseTheValueForTheKeyInSecondMapAndTheKeyForTheValueInFirstMap(
+  hash1,
+  hash2
+) {
+  let newHash = {};
+  let keys = Object.keys(hash1);
+  console.log(hash1);
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    newHash[hash2[key]] = key;
+  }
+  console.log("Reassigned Key/Value pair: ", newHash);
+  return newHash;
+}
+createHashAndUseTheValueForTheKeyInSecondMapAndTheKeyForTheValueInFirstMap(
+  { a: 1, b: 2 },
+  { a: 1, b: 2 }
+); // expects {a: 1, b: 2} => {1: a, 2: b}
