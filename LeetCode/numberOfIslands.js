13ca8cf9dbc87df6b14005ed937c579329c51d84
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-12-11 16:10:52 -0600

    update numberOfIslands.js

--- LeetCode/numberOfIslands.js
+++ LeetCode/numberOfIslands.js
@@ -2,6 +2,7 @@
 // https://leetcode.com/problems/number-of-islands/
 
 function numIslands(grid) {
+  // Solution 1
   let count = 0;
   for (let r = 0; r < grid.length; r++) {
     for (let c = 0; c < grid[0].length; c++) {
@@ -23,6 +24,36 @@ function numIslands(grid) {
     }
   }
   return count;
+
+  // Solution 2 using DFS
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === "1") {
+        count++;
+        dfs(grid, i, j);
+      }
+    }
+  }
+  console.log("count: ", count);
+  return count;
+
+  function dfs(grid, i, j) {
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      j < 0 ||
+      j >= grid[i].length ||
+      grid[i][j] === "0"
+    )
+      return;
+
+    grid[i][j] = "0";
+    dfs(grid, i - 1, j);
+    dfs(grid, i + 1, j);
+    dfs(grid, i, j - 1);
+    dfs(grid, i, j + 1);
+  }
 }
 numIslands([
   ["1", "1", "1", "1", "0"],
