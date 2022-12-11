829898b2ae34e8993c9b9c83fcc0ae5910bdaac3
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-12-11 16:08:22 -0600

    LeetCode

--- LeetCode/reconstructOriginalDigitsFromEnglish.js
+++ LeetCode/reconstructOriginalDigitsFromEnglish.js
@@ -3,106 +3,106 @@
 
 var originalDigits = function (s) {
   // Solution 1
-  // const map = {};
+  const map = {};
 
-  // for (let i = 0; i < s.length; i++) {
-  //   if (map[s[i]] === undefined) {
-  //     map[s[i]] = 1;
-  //   } else map[s[i]]++;
-  // }
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] === undefined) {
+      map[s[i]] = 1;
+    } else map[s[i]]++;
+  }
 
-  // const result = [];
-  // const zero = map["z"];
-  // if (zero > 0) {
-  //   result.push("0".repeat(zero));
-  //   map["z"] -= zero;
-  //   map["e"] -= zero;
-  //   map["r"] -= zero;
-  //   map["o"] -= zero;
-  // }
+  const result = [];
+  const zero = map["z"];
+  if (zero > 0) {
+    result.push("0".repeat(zero));
+    map["z"] -= zero;
+    map["e"] -= zero;
+    map["r"] -= zero;
+    map["o"] -= zero;
+  }
 
-  // const two = map["w"];
-  // if (two > 0) {
-  //   result.push("2".repeat(two));
-  //   map["t"] -= two;
-  //   map["w"] -= two;
-  //   map["o"] -= two;
-  // }
+  const two = map["w"];
+  if (two > 0) {
+    result.push("2".repeat(two));
+    map["t"] -= two;
+    map["w"] -= two;
+    map["o"] -= two;
+  }
 
-  // const four = map["u"];
-  // if (four > 0) {
-  //   result.push("4".repeat(four));
-  //   map["f"] -= four;
-  //   map["o"] -= four;
-  //   map["u"] -= four;
-  //   map["r"] -= four;
-  // }
+  const four = map["u"];
+  if (four > 0) {
+    result.push("4".repeat(four));
+    map["f"] -= four;
+    map["o"] -= four;
+    map["u"] -= four;
+    map["r"] -= four;
+  }
 
-  // const six = map["x"];
-  // if (six > 0) {
-  //   result.push("6".repeat(six));
-  //   map["s"] -= six;
-  //   map["i"] -= six;
-  //   map["x"] -= six;
-  // }
+  const six = map["x"];
+  if (six > 0) {
+    result.push("6".repeat(six));
+    map["s"] -= six;
+    map["i"] -= six;
+    map["x"] -= six;
+  }
 
-  // const eight = map["g"];
-  // if (eight > 0) {
-  //   result.push("8".repeat(eight));
-  //   map["e"] -= eight;
-  //   map["i"] -= eight;
-  //   map["g"] -= eight;
-  //   map["h"] -= eight;
-  //   map["t"] -= eight;
-  // }
+  const eight = map["g"];
+  if (eight > 0) {
+    result.push("8".repeat(eight));
+    map["e"] -= eight;
+    map["i"] -= eight;
+    map["g"] -= eight;
+    map["h"] -= eight;
+    map["t"] -= eight;
+  }
 
-  // const one = map["o"];
-  // if (one > 0) {
-  //   result.push("1".repeat(one));
-  //   map["o"] -= one;
-  //   map["n"] -= one;
-  //   map["e"] -= one;
-  // }
+  const one = map["o"];
+  if (one > 0) {
+    result.push("1".repeat(one));
+    map["o"] -= one;
+    map["n"] -= one;
+    map["e"] -= one;
+  }
 
-  // const three = map["h"];
-  // if (three > 0) {
-  //   result.push("3".repeat(three));
-  //   map["t"] -= three;
-  //   map["h"] -= three;
-  //   map["r"] -= three;
-  //   map["e"] -= three;
-  //   map["e"] -= three;
-  // }
+  const three = map["h"];
+  if (three > 0) {
+    result.push("3".repeat(three));
+    map["t"] -= three;
+    map["h"] -= three;
+    map["r"] -= three;
+    map["e"] -= three;
+    map["e"] -= three;
+  }
 
-  // const five = map["f"];
-  // if (five > 0) {
-  //   result.push("5".repeat(five));
-  //   map["f"] -= five;
-  //   map["i"] -= five;
-  //   map["v"] -= five;
-  //   map["e"] -= five;
-  // }
+  const five = map["f"];
+  if (five > 0) {
+    result.push("5".repeat(five));
+    map["f"] -= five;
+    map["i"] -= five;
+    map["v"] -= five;
+    map["e"] -= five;
+  }
 
-  // const seven = map["s"];
-  // if (seven > 0) {
-  //   result.push("7".repeat(seven));
-  //   map["s"] -= seven;
-  //   map["e"] -= seven;
-  //   map["v"] -= seven;
-  //   map["e"] -= seven;
-  //   map["n"] -= seven;
-  // }
+  const seven = map["s"];
+  if (seven > 0) {
+    result.push("7".repeat(seven));
+    map["s"] -= seven;
+    map["e"] -= seven;
+    map["v"] -= seven;
+    map["e"] -= seven;
+    map["n"] -= seven;
+  }
 
-  // const nine = map["i"];
-  // if (nine > 0) {
-  //   result.push("9".repeat(nine));
-  //   map["n"] -= nine;
-  //   map["i"] -= nine;
-  //   map["n"] -= nine;
-  //   map["e"] -= nine;
-  // }
-  // console.log(result.join(""));
-  // return result.join("");
+  const nine = map["i"];
+  if (nine > 0) {
+    result.push("9".repeat(nine));
+    map["n"] -= nine;
+    map["i"] -= nine;
+    map["n"] -= nine;
+    map["e"] -= nine;
+  }
+  console.log(result.join(""));
+  return result.join("");
 
   // Solution 2
   let numsCount = {
@@ -140,7 +140,7 @@ var originalDigits = function (s) {
 
   // console.log("NEW NUMS COUNT:", numsCount);
 
-  let result = "";
+  // let result = "";
 
   for (let j = 0; j < 10; j++) {
     while (numsCount[j] !== 0) {
