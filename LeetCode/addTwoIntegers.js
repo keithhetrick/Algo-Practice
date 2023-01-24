540d23a407605a6de1b28d21fa2aad862c497e3f
Keith Hetrick <keithjhetrick@gmail.com>
2023-01-24 00:56:49 -0600

    Leetcode

---
+++ LeetCode/addTwoIntegers.js
@@ -0,0 +1,11 @@
+// 2235. Add Two Integers
+// https://leetcode.com/problems/add-two-integers/description/
+
+// Given two integers num1 and num2, return the sum of the two integers.
+
+var sum = function (num1, num2) {
+  console.log("\nSUM: ", sum);
+  return num1 + num2;
+};
+sum(12, 5); // expects 17
+sum(-10, 4); // expects -6
