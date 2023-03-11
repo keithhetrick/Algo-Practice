b8a6930fb6ff4b8b509083a2ead0073873f4de98
Keith Hetrick <keithjhetrick@gmail.com>
2023-03-11 09:52:18 -0600

    misc update

--- misc/misc-Practice/objectPractice.js
+++ misc/misc-Practice/objectPractice.js
@@ -38,6 +38,7 @@ function person(first_name, last_name) {
   this.first_name = first_name;
   this.last_name = last_name;
 }
+
 //creating new instances of person object
 let person1 = new person("Mukul", "Latiyan");
 let person2 = new person("Rahul", "Avasthi");
