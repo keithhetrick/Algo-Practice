da38880217962f454466f65309e8d521d9a78a45
Keith Hetrick <keithjhetrick@gmail.com>
2023-03-15 23:59:05 -0400

    algo practice

--- misc/misc-Practice/randomFreestyleCoding.js
+++ misc/misc-Practice/randomFreestyleCoding.js
@@ -14,8 +14,6 @@ function pullLettersOut(matrix) {
   let newArr = [];
 
   for (let i = 0; i < matrix.length; i++) {
-    // newArr.push(matrix[i][0]);
-    // console.log(matrix[i][0]);
     console.log("MATRIX[i][i]: ", matrix[i][i]);
 
     if (matrix[i] % 2 !== 0) {
@@ -29,3 +27,19 @@ pullLettersOut([
   [6, 7, 8, 9],
   [10, 11, 12, 13, 14],
 ]); // expects [1, 6, 10]
+
+function reverseThrougMatrix(matrix) {
+  let newArr = [];
+
+  for (let i = matrix.length - 1; i >= 0; i--) {
+    for (let j = matrix[i].length - 1; j >= 0; j--) {
+      newArr.push(matrix[i][j]);
+    }
+  }
+  return console.log("\nreverseThrougMatrix\tNEW ARRAY: ", newArr);
+}
+reverseThrougMatrix([
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9],
+  [10, 11, 12, 13, 14],
+]); // expects [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
